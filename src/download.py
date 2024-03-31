4ecbdccba26125e9c40471ec28557126a5b7550d
Mikeprod <mikeprod.tm@gmail.com>
2024-03-31 13:54:28 +0200

    feat: improve the download experience

--- src/download.py
+++ src/download.py
@@ -1,9 +1,42 @@
 import re
-import time
+from pathlib import Path
+from urllib.error import URLError
 from urllib.request import urlopen
 
-# Paste your sash stream url here
+from tqdm import tqdm
+
+# Paste your dash stream url here
 stream = ""
+
+
+def is_url_available(url: str) -> bool:
+    """Check if the url is available.
+
+    :param url: url to check
+    :type url: str
+    :return: True if the url is available, False otherwise
+    :rtype: bool
+    """
+    try:
+        urlopen(url)
+        return True
+    except URLError:
+        return False
+
+
+def define_segment_range(url: str, segment_step: int) -> tuple[int, int]:
+    """Define the range of the segments.
+
+    :param url: url of the dash file
+    :type url: str
+    :param segment_step: step between each segment
+    :type segment_step: int
+    :return: tuple of the range of the segments
+    :rtype: tuple
+    """
+    for i in range(0, 1000000000, segment_step):
+        if not is_url_available(url.replace("-0.dash", f"-{i}.dash")):
+            return 0, i
 
 
 def download_dashed_vtt(url: str, dl_dir: str, segment_step: int = 10000, segment_size: int = 1000) -> None:
@@ -11,6 +44,8 @@ def download_dashed_vtt(url: str, dl_dir: str, segment_step: int = 10000, segmen
 
     :param url: url of the dash file
     :type url: str
+    :param dl_dir: directory to download the dash files
+    :type dl_dir: str
     :param segment_step: step between each segment, defaults to 10000
     :type segment_step: int, optional
     :param segment_size: size of the segment, defaults to 1000
@@ -19,14 +54,16 @@ def download_dashed_vtt(url: str, dl_dir: str, segment_step: int = 10000, segmen
     :rtype: None
     """
     segment_url = re.sub(r"qsm=\d+-", f"qsm={segment_size}-", url)
-    for i in range(0, 1000000000, segment_step):
-        with urlopen(
-                segment_url.replace("-0.dash", f"-{i}.dash")
-        ) as data:
+    destination = Path(dl_dir)
+    destination.mkdir(parents=True, exist_ok=True)
+
+    print(f"Downloading segments to {destination.absolute()}")
+    print("After evaluating the number of segments, the download will start.")
+    for i in tqdm(range(*define_segment_range(segment_url, segment_step), segment_step)):
+        with urlopen(segment_url.replace("-0.dash", f"-{i}.dash")) as data:
             with open(f"{dl_dir}/{i:08d}.mp4", "wb") as writer:
                 writer.write(data.read())
-        time.sleep(0.5)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     download_dashed_vtt(stream, "dash", 10000)
